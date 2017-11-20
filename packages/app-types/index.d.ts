790f05650577d3bffe71ef906bebde9817dfd59b
Josh Crowther <jshcrowthe@gmail.com>
2017-11-20 11:33:39 -0800

    Adding base file for `@firebase/app-types`

---
+++ packages/app-types/index.d.ts
@@ -0,0 +1,28 @@
+export type FirebaseOptions = {
+  apiKey?: string;
+  authDomain?: string;
+  databaseURL?: string;
+  projectId?: string;
+  storageBucket?: string;
+  messagingSenderId?: string;
+  [name: string]: any;
+};
+
+export interface FirebaseApp {
+  /**
+   * The (read-only) name (identifier) for this App. '[DEFAULT]' is the default
+   * App.
+   */
+  name: string;
+
+  /**
+   * The (read-only) configuration options from the app initialization.
+   */
+  options: FirebaseOptions;
+  
+  /**
+   * Make the given App unusable and free resources.
+   */
+  delete(): Promise<void>;
+}
+
